09a477ab5f92b49ee67fe7d65b8f0e6b7fc56a4f
kelj0 <kelj0@protonmail.com>
2023-04-14 02:39:57 +0200

    rename project

--- dr_buster_web_project/drb_web/asgi.py
+++ dr_buster_web_project/drb_web/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for dr_buster_web_project project.
+ASGI config for drb_web project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dr_buster_web_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drb_web.settings')
 
 application = get_asgi_application()
--- dr_buster_web_project/drb_web/wsgi.py
+++ dr_buster_web_project/drb_web/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dr_buster_web_project project.
+WSGI config for drb_web project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dr_buster_web_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drb_web.settings')
 
 application = get_wsgi_application()
