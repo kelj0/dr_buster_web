411346821c788e6bba83ee0dfa5899ea8ffca172
kelj0 <kelj0@protonmail.com>
2023-04-14 02:31:36 +0200

    finish MVP

--- dr_buster_web_project/webapp/views.py
+++ dr_buster_web_project/webapp/views.py
@@ -5,50 +5,104 @@ from dr_buster.core import main as dr_buster_scan
 from webapp.models import Scan, Wordlist
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
+from django.core.exceptions import ObjectDoesNotExist
+import os
 
-
+@swagger_auto_schema(
+    method='post',
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'wordlist_id': openapi.Schema(
+                type=openapi.TYPE_STRING,
+                description='ID of the wordlist to use',
+            ),
+            'url': openapi.Schema(
+                type=openapi.TYPE_STRING,
+                description='url of the targeted server'
+            ),
+        },
+        required=['wordlist_id', 'url'],
+    ),
+)
 @api_view(['POST'])
 def dr_buster_start_scan(request):
     '''
         Starts a new scan, returns id of a saved scan that you can use in get_result
-        parameters:
-        - wordlist_id: wordlist id that you got after uploading new wordlist
-        - url: url that you want to scan
     '''
     # Get the parameters from the request
     wordlist_id = request.data.get('wordlist_id')
     target_url = request.data.get('url')
     
-    wordlist = Wordlist.objects.get(id=wordlist_id)
-    # create wordlist on path start stan rm from path
-    wordlist_path = "" 
-    # Run dr_buster with the given parameters
-    report_path = dr_buster_scan(wordlist_path, target_url, cli_run=False)
-    lines = open(report_path).readlines()
-    count = len(lines)
+    wordlist = Wordlist.objects.get(id=wordlist_id).file.decode("UTF-8")
+    dirpath = os.path.dirname(os.path.realpath(__file__))
+    wordlist_path = os.path.join(dirpath, 'temp_wordlists', f"{wordlist_id.replace('-', '')}.txt")
+    with open(wordlist_path, 'w') as f:
+        f.write(wordlist)
+
+    report_path = dr_buster_scan(
+        url=target_url,
+        wordlist_path=wordlist_path,
+        cli_run=False
+    )
 
     scan = Scan()
-    if count > 0:
+    if not os.path.exists(report_path):
         scan.result = {
-            'count': count,
-            'urls': lines
+            'count': 0,
+            'urls': ''
         }
-    saved_scan = scan.save()
+    else:
+        with open(report_path) as f:
+            lines = f.readlines()
+            scan.result = {
+                'count': len(lines),
+                'urls': lines
+            }
+    
+    scan.save()
 
-    return Response({'id': saved_scan.id})
+    return Response({'id': scan.id})
 
 
-
+@swagger_auto_schema(
+    method='post',
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        required=['file'],
+        properties={
+            'file': openapi.Schema(
+                type=openapi.TYPE_FILE,
+                description='Wordlist file'
+            )
+        },
+    ),
+    responses={
+        200: openapi.Response(description='ID of the saved wordlist', schema=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'id': openapi.Schema(
+                    type=openapi.TYPE_INTEGER,
+                    description='ID of the saved wordlist'
+                )
+            },
+        )),
+        400: openapi.Response(description='No file was provided', schema=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'error': openapi.Schema(
+                    type=openapi.TYPE_STRING,
+                    description='Error message'
+                )
+            },
+        ))
+    }
+)
 @api_view(['POST'])
 def upload_wordlist(request):
     '''
         Upload a new wordlist to service
         CLI usage: curl -X POST -F "file=@/path/to/wordlist.txt" http://localhost:8000/upload_wordlist/
-        parameters:
-        - file: file
-        description: file that you want to upload
-        required: true
-        type: file
     '''
 
     file = request.FILES.get('file')
@@ -67,8 +121,14 @@ def get_available_wordlists(request):
         Fetch all used wordlists
     '''
     wordlists = Wordlist.objects.all()
-    data = serializers.serialize('json', wordlists)
-    return Response(data)
+    wordlists = [
+            {
+                'id': wordlist.id,
+                'file': wordlist.file.decode('UTF-8')
+            } 
+        for wordlist in wordlists
+    ]
+    return Response(wordlists)
 
 
 @swagger_auto_schema(
@@ -88,9 +148,9 @@ def get_result(request):
     '''
         Fetch results of a scan 
     '''
-    scan_id = request.query.get('scan_id')
-    scan = Scan.objects.query(id=scan_id)
-    if scan:
-        data = serializers.serialize('json', scan.result)
-        return Response(data)
-    return Response('Scan not found', 404)
+    scan_id = request.query_params.get('scan_id')
+    try:
+        scan = Scan.objects.get(id=scan_id)
+        return Response(scan.result)
+    except ObjectDoesNotExist:
+        return Response('Scan not found', 404)
